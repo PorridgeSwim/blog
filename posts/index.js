33e456a323efe7d2e628c79f67135f0e28a9a42e
You Zhou <yozhou24@YoudeMacBook-Air.local>
2023-12-30 00:42:58 -0800

    implement event bus

--- posts/index.js
+++ posts/index.js
@@ -4,6 +4,7 @@ const { randomBytes } = require('crypto');
 const app = express();
 const post = {};
 const cors = require('cors');
+const axios = require('axios');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,7 +13,7 @@ app.get('/posts', (req, res) => {
     res.send(post);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -20,9 +21,21 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
-    res.status(201).send(post[id]);
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, 
+            title
+        }
+    });
 
+    res.status(201).send(post[id]);
 });
+
+app.post('/events', (req, res) => {
+    console.log('Recieved Event', req.body.type);
+    res.send({});
+})
 
 app.listen(4000, () => {
     console.log('Listening on 4000');
